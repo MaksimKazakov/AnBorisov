3d4a22655cde3481ce76a001fc25264b1ecacf59
KazakovMS <info@kazakovms.ru>
2023-11-15 00:02:29 +0500

    6 домашка

---
+++ src/HomeWorkSixth.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HomeWorkSixth {
+    public static void main(String[] args) {
+        System.out.print("Введите пятизначное число => ");
+        Scanner scaner = new Scanner(System.in);
+        int number = scaner.nextInt();
+        System.out.println("Третья цифра числа  = " + getNumber(number));
+    }
+
+    static int getNumber(int number){
+        return (number / 100) % 10;
+    }
+}
